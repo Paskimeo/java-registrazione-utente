e4c9455612b2ba8cd8f5e2e85dcdd0731e4a7ec8
paskimeo <paskimeo@gmail.com>
2022-06-24 15:47:05 +0200

    aggiunti metodi di validazione

--- src/raccoltaDati/Utente.java
+++ src/raccoltaDati/Utente.java
@@ -9,8 +9,26 @@ public class Utente {
 	
 	
 	//costruttore
-	public Utente(String nome, String cognome, String email, String password, int eta) {
-		super();
+	public Utente(String nome, String cognome, String email, String password, int eta) throws Exception  {
+	
+		//metodi di validazione
+		if(eta < 18){
+		      throw new Exception("Km deve essere maggiore di 0");
+	    }
+		
+		 if ( !(email.contains("@")) || !(email.contains(".") )) {
+	            throw new Exception("La mail deve contenere un '@' e un '.'");
+		 
+		 }
+		 
+	     if (password.length()<8 || password.length()>12) {
+	        throw new Exception("La password deve contenere tra gli 8 e i 12 caratteri");
+	     }
+		
+		
+		
+		
+		//this del costruttore
 		this.nome = nome;
 		this.cognome = cognome;
 		this.email = email;
